1582218492f1df1e871ea099a8c3b90363cbe313
mick-rudolph <69992187+mick-rudolph@users.noreply.github.com>
2021-04-08 15:41:33 -0600

    Major changes to rwd home page

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
 $(document).ready(function(){
 });
 
-$('.intro-box').animate({width: 0, marginRight: 0}, {duration: 1000});
-$(window).on('load', function() {
-    $('.intro-box').animate({width: "400px", marginRight:0},500,"swing",function(){
-        $('.intro').css({"opacity":"1"});
-            $('.intro-box').animate({width: 0, right: "-400px"}, {duration: 2000}, "swing",);
-            $('.intro-box').animate({width: "130px", marginRight:0},600,"swing",function(){
-                $('.intro-box2').fadeIn(2500);
-            });
-            $('.intro-subtext').fadeIn(5000,function(){
-                $('#hero-button').fadeIn(2000);
-                $('.view-projects').fadeIn(5000);
-            });
-    });
+// $(window).on('load', function() {
+//     $('#hero-button').fadeIn(2500,function(){
+//         $('.view-projects').fadeIn(2000);
+//     });
+// });
+
+
+$( window ).resize(function() {
+    if ($(window).width() < 768 ) {
+       $('.reverse').addClass('flex-column-reverse')
+    } else {
+        $('.reverse').removeClass('flex-column-reverse');
+    }
 });
-
-
-
-// var observer = new IntersectionObserver(function(entries) { console.log(entries);
-// 	if(entries[0]['isIntersecting'] === true) {
-// 		if(entries[0]['intersectionRatio'] === 1)
-// 			document.querySelector("#message").textContent = 'Target is fully visible in screen';
-// 		else if(entries[0]['intersectionRatio'] > 0.5)
-// 			document.querySelector("#message").textContent = 'More than 50% of target is visible in screen';
-// 		else
-// 			document.querySelector("#message").textContent = 'Less than 50% of target is visible in screen';
-// 	}
-// 	else {
-// 		document.querySelector("#message").textContent = 'Target is not visible in screen';
-// 	}
-// }, { threshold: [0, 0.5, 1] });
-
-// observer.observe(document.querySelector("#target-container"));
-
-
-
 
 $.fn.isInViewport = function () {
     let elementTop = $(this).offset().top;
@@ -75,3 +54,19 @@ $(window).scroll(function () {
 
 
 
+
+// var observer = new IntersectionObserver(function(entries) { console.log(entries);
+// 	if(entries[0]['isIntersecting'] === true) {
+// 		if(entries[0]['intersectionRatio'] === 1)
+// 			document.querySelector("#message").textContent = 'Target is fully visible in screen';
+// 		else if(entries[0]['intersectionRatio'] > 0.5)
+// 			document.querySelector("#message").textContent = 'More than 50% of target is visible in screen';
+// 		else
+// 			document.querySelector("#message").textContent = 'Less than 50% of target is visible in screen';
+// 	}
+// 	else {
+// 		document.querySelector("#message").textContent = 'Target is not visible in screen';
+// 	}
+// }, { threshold: [0, 0.5, 1] });
+
+// observer.observe(document.querySelector("#target-container"));
